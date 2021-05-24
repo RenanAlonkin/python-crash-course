bcf42febd1c897c73c232bebb2dead50cdcb627c
Renan Nunes Steinck <renan.alonkin@gmail.com>
2021-05-23 22:05:01 -0300

    Adding the first two do it yourselfs of the book

---
+++ chapter_two/do_it_yourselfs/simple_messages.py
@@ -0,0 +1,7 @@
+# This file has the objective to use the right naming convention for Python
+error_code = "404"
+print(error_code)
+
+# Altering a variable value
+error_code = "404 - Page not found"
+print(error_code)
